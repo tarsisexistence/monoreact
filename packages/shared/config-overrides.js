7f376023824490e37213bb3b6fed1f4af4cefb9f
maktarsis <tarsis.maksym@gmail.com>
2020-02-03 15:46:04 +0200

    stable checkpoint

--- packages/shared/config-overrides.js
+++ packages/shared/config-overrides.js
@@ -1,60 +1,55 @@
-// process.env.NODE_ENV = 'development';
-
-const fs = require('fs-extra');
-const paths = require('react-scripts/config/paths');
-const path = require('path');
-// const webpack = require('webpack');
-// const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
-//
-// module.exports = function override(config, env) {
-//   config.entry = config.entry.filter(
-//       entry => !entry.includes('webpackHotDevClient'));
-//   config.output.publicPath = process.env.PUBLIC_URL;
-//   // config.output.path = paths.appBuild;
-//   paths.publicUrl = paths.appBuild + '/';
-//   config.output.library = 'example';
-//   config.output.libraryTarget = 'commonjs-module';
-//
-//   webpack(config).watch({
-//     mode: 'development',
-//     cache: false,
-//     plugins: [  // Array of plugins to apply to build chunk
-//       new HtmlWebpackPlugin({
-//         template: '',
-//       }),
-//     ],
-//   }, () => {
-//   });
-//
-//   return config;
-// };
-
 const webpack = require('webpack');
+const paths = require('react-scripts/config/paths');
 
-module.exports = function override(config) {
-  config.output.filename = 'static/js/[name].js';
-  config.output.chunkFilename = 'static/js/[name].chunk.js';
-  config.output.library = 'MyLib';
+module.exports = function override(config, env) {
+  const cssOptionId = config.plugins.findIndex(
+      plugin => plugin.options && plugin.options.filename &&
+          plugin.options.filename.includes('css'));
+
+  if (cssOptionId !== -1) {
+    config.plugins[cssOptionId].filename = 'static/css/[name].css';
+    config.plugins[cssOptionId].chunkFilename = 'static/css/[name].css';
+  }
+
+  if (env === 'production') {
+    config.output.filename = 'static/js/[name].js';
+    config.output.chunkFilename = 'static/js/[name].chunk.js';
+    // check does it work
+    config.externals = [
+      'react',
+      'react-dom',
+      '/^react\\/.+$/',
+    ];
+    // react: {
+    //   commonjs: 'react',
+    //   commonjs2: 'react',
+    //   umd: 'react',
+    //   root: 'react',
+    // },
+    // 'react-dom': {
+    //   commonjs: 'react-dom',
+    //   commonjs2: 'react-dom',
+    //   umd: 'react-dom',
+    //   root: 'react-dom'
+    // },
+  } else {
+    paths.publicUrl = `${paths.appBuild}/`;
+    config.output.publicPath = paths.publicUrl;
+
+    config.entry = config.entry.filter(
+        entry => !entry.includes('webpackHotDevClient'));
+    config.output.publicPath = '/';
+
+    webpack(config).watch({
+      mode: 'development',
+      cache: false,
+    }, () => {
+    });
+  }
+
+  config.output.library = 'omniaShared';
   config.output.libraryTarget = 'umd';
   config.output.umdNamedDefine = true;
 
-  config.output.publicPath =   path.resolve(__dirname, 'dist')
-  config.output.path = path.resolve(__dirname, 'dist');
-  paths.publicUrl = path.resolve(__dirname, 'dist');
-
-  console.log(config);
-
-  webpack(config).watch({
-    watchOptions: 3500,
-  }, (err, stats) => {
-    if (err) {
-      console.error(err);
-    }
-
-    console.error(stats.toString({
-      chunks: false,
-      colors: true,
-    }));
-  });
   return config;
 };
