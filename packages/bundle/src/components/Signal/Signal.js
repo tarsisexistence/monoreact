923685515628a8f004b5f6d16ad9654bf71fbbf3
maktarsis <tarsis.maksym@gmail.com>
2020-02-08 00:22:41 +0200

    remove bundle dir

--- packages/bundle/src/components/Signal/Signal.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './Signal.module.css';
-
-export const Signal = () => (
-    <article className={styles.container}>Signal</article>
-);
--- packages/bundle/src/components/index.js
+++
@@ -1,2 +0,0 @@
-export * from './Alert/Alert';
-export * from './Signal/Signal';
