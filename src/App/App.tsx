2b0f2ebad8921a8d93b1cbbe5f5b5f91577c248d
maktarsis <tarsis.maksym@gmail.com>
2020-02-09 11:56:59 +0200

    yarn workspaces introduction

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Products } from '@tarsis/starter';
+
 import styles from './App.module.css';
 
-export const App = () => <div className={styles.container}>Container</div>;
+export const App = () => (
+  <div className={styles.container}>
+    <Products />
+  </div>
+);
