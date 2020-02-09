0f02163cc67b58ac68b1e76115f0352814bb25ec
maktarsis <tarsis.maksym@gmail.com>
2020-02-09 13:49:11 +0200

    fix styles

--- packages/starter/src/components/ProductsList/ProductsList.tsx
+++ packages/starter/src/components/ProductsList/ProductsList.tsx
@@ -1,21 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Box, Grommet, grommet, List } from 'grommet';
 
 import { productsProgress } from './ProductsList.helpers';
 import styles from './ProductsList.scss';
 
-export const ProductsList = () => {
-	const [, setClicked] = useState();
-
-	return (
-		<Grommet theme={grommet}>
-			<Box align="center" pad="medium" className={styles.itemList}>
-				<List data={productsProgress}
-							onClickItem={event => setClicked(event.item)}
-							primaryKey={item => item.name}
-							secondaryKey={item => item.percent}
-				/>
-				</Box>
-			</Grommet>
-	);
-};
+export const ProductsList = () => (
+	<Grommet theme={grommet} className={styles.container}>
+		<Box align="center" pad="medium" className={styles.list}>
+			<List data={productsProgress}
+						primaryKey={item => item.name}
+						secondaryKey={item => `${item.percent}%`}
+			/>
+		</Box>
+	</Grommet>
+);
