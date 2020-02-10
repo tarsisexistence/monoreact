f2dae2e159840ed2ed4bc39c35567b59a0a6c52c
maktarsis <tarsis.maksym@gmail.com>
2020-02-10 14:32:16 +0200

    refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App/App';
 import { unregister } from './serviceWorker';
-import './styles.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
