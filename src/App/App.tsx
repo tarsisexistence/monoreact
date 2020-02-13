bec999e70f7af1f0d2bd797fce3202130316c5bf
maktarsis <tarsis.maksym@gmail.com>
2020-02-13 15:04:24 +0200

    all package specific devDeps to main package json

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Products } from '@tarsis/starter';
 
+import { Products } from '@tarsis/starter';
 import styles from './App.module.css';
 
 export const App = () => (
