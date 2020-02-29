fceaa2ddeb8eca5be8d8465e18661893b469afee
maktarsis <tarsis.maksym@gmail.com>
2020-02-29 19:26:12 +0200

    chore: refactoring

---
+++ packages/cli/templates/basic/src/Component.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Component.scss';
+
+export const Component = () => (
+  <section className={styles.container}>Component</section>
+);
--- packages/example/playground/index.tsx
+++ packages/example/playground/index.tsx
@@ -1,6 +1,5 @@
-import 'react-app-polyfill/ie11';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { Products } from '..';
 
 const App = () => {
