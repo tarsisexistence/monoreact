4cbe8cd6be386bae244d8d03fc894b050407630d
maktarsis <tarsis.maksym@gmail.com>
2020-03-01 19:38:07 +0200

    refactor: remove redundant code

--- cli/src/utils.ts
+++ cli/src/utils.ts
@@ -1,27 +1,10 @@
-import fs from 'fs-extra';
-import path from 'path';
-import camelCase from 'camelcase';
 import shell from 'shelljs';
 import { Author } from './types';
-
-// Remove the package name scope if it exists
-export const removeScope = (name: string) => name.replace(/^@.*\//, '');
-
-// UMD-safe package name
-export const safeVariableName = (name: string) =>
-  camelCase(
-    removeScope(name)
-      .toLowerCase()
-      .replace(/((^[^a-zA-Z]+)|[^\w.-])|([^a-zA-Z0-9]+$)/g, '')
-  );
 
 export const safePackageName = (name: string) =>
   name
     .toLowerCase()
     .replace(/(^@.*\/)|((^[^a-zA-Z]+)|[^\w.-])|([^a-zA-Z0-9]+$)/g, '');
-
-export const resolveApp = (relativePath: string) =>
-  path.resolve(fs.realpathSync(process.cwd()), relativePath);
 
 export function getAuthorName(): Author {
   let author = '';
