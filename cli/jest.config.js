05cb617dd8b38dc50f698707ad420725f18fdb7f
maktarsis <tarsis.maksym@gmail.com>
2020-03-21 21:15:23 +0200

    fix: baseUrl for testing

--- cli/jest.config.js
+++ cli/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   ...jestPackageConfig,
   roots: [path.resolve(__dirname, 'src')],
   setupFiles: [path.resolve(__dirname, '../settings/setupTests.ts')],
+  moduleDirectories: ['node_modules', 'src'],
   testPathIgnorePatterns: [
     path.resolve(__dirname, 'node_modules/'),
     path.resolve(__dirname, '../node_modules/')
