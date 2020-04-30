2c06b216089755a0d0d10964e10c4608a94ac96b
maktarsis <tarsis.maksym@gmail.com>
2020-04-30 22:56:49 +0300

    chore: update templates

--- cli/templates/generate/basic/.eslintrc.js
+++ cli/templates/generate/basic/.eslintrc.js
@@ -1,20 +1,5 @@
 const path = require('path');
 
 module.exports = {
-  extends: path.resolve(__dirname, '../../.eslintrc.js'),
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: [
-      path.resolve(__dirname, '../../settings/tsconfig.lint.json'),
-      path.resolve(__dirname, 'tsconfig.json')
-    ]
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        paths: [path.resolve(__dirname, 'src')],
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      }
-    }
-  }
+  extends: path.resolve(__dirname, '../../.eslintrc.js')
 };
