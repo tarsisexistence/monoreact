960fb86f4e4035a73448356fcfea20d1b453d6f1
maktarsis <tarsis.maksym@gmail.com>
2020-05-08 09:41:34 +0300

    refactor: update eslint

--- cli/templates/generate/basic/.eslintrc.js
+++ cli/templates/generate/basic/.eslintrc.js
@@ -1,5 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  extends: path.resolve(__dirname, '../../.eslintrc.js')
+  extends: path.resolve(__dirname, '../../.eslintrc.js'),
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true
+    }
+  }
 };
