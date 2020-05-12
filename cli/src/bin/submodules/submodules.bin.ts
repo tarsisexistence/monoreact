d16a61b5ec46c4ad5db32f4aab2e9286b29fe286
maktarsis <tarsis.maksym@gmail.com>
2020-05-12 22:44:30 +0300

    refactor: remove redundant submodules build script

--- cli/src/bin/submodules/submodules.bin.ts
+++ cli/src/bin/submodules/submodules.bin.ts
@@ -1,13 +1,11 @@
 import { Sade } from 'sade';
 
-import { submodulesBuildBinCommand } from './submodules-build.bin';
 import { submodulesCheckoutBinCommand } from './submodules-checkout.bin';
 import { submodulesFetchBinCommand } from './submodules-fetch.bin';
 import { submodulesInitBinCommand } from './submodules-init.bin';
 import { submodulesPullBinCommand } from './submodules-pull.bin';
 
 export const submodulesBinCommand = (prog: Sade) => {
-  submodulesBuildBinCommand(prog);
   submodulesCheckoutBinCommand(prog);
   submodulesFetchBinCommand(prog);
   submodulesInitBinCommand(prog);
--- cli/src/typings/submodules.d.ts
+++ cli/src/typings/submodules.d.ts
@@ -1,3 +1,3 @@
 declare namespace CLI.Submodules {
-  type Command = 'init' | 'fetch' | 'pull' | 'build' | 'checkout';
+  type Command = 'init' | 'fetch' | 'pull' | 'checkout';
 }
