543cf48c964edb44d48046d6bd7d572af14158f1
maktarsis <tarsis.maksym@gmail.com>
2020-05-31 10:09:26 +0300

    fix: code issues

--- cli/src/shared/utils/index.ts
+++ cli/src/shared/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './color.utils';
 export * from './common.utils';
 export * from './dependency.utils';
-export * from './package.utils';
 export * from './error.utils';
 export * from './workspace.utils';
