70a029be011390326ffc328875e87dcf7fb4caf5
maktarsis <tarsis.maksym@gmail.com>
2020-06-05 20:01:09 +0300

    chore: refactoring, patch release

--- cli/src/bin/execution/configs/build.config.ts
+++ cli/src/bin/execution/configs/build.config.ts
@@ -32,93 +32,89 @@ export const createBuildConfig = ({
   runEslint: boolean;
   packageJson: CLI.Package.WorkspacePackageJSON;
   tsconfigJson: TsconfigJSON;
-}): InputOptions & { output: OutputOptions } => {
-  const externalScreen = getExternalScreen(packageJson);
-
-  return {
-    input: packageJson.source,
-    output: {
-      file: packageJson.module,
-      format: 'es',
-      sourcemap: true
-    },
-    external: externalScreen,
-    plugins: [
-      beep(),
-      progress({ clearLine: true }),
-      runEslint && eslint(),
-      displayFilesize && filesize(),
-      useClosure && closure(),
-      json(),
-      url(),
-      image(),
-      postcss({
-        extract: false,
-        modules: true,
-        writeDefinitions: true,
-        plugins: [
-          simplevars({ variables: {} }),
-          nested(),
-          cssnano(),
-          autoprefixer()
+}): InputOptions & { output: OutputOptions } => ({
+  input: packageJson.source,
+  output: {
+    file: packageJson.module,
+    format: 'es',
+    sourcemap: true
+  },
+  external: getExternalScreen(packageJson),
+  plugins: [
+    beep(),
+    progress({ clearLine: true }),
+    runEslint && eslint(),
+    displayFilesize && filesize(),
+    useClosure && closure(),
+    json(),
+    url(),
+    image(),
+    postcss({
+      extract: false,
+      modules: true,
+      writeDefinitions: true,
+      plugins: [
+        simplevars({ variables: {} }),
+        nested(),
+        cssnano(),
+        autoprefixer()
+      ],
+      extensions: ['.css', '.scss', '.sass'],
+      use: ['sass']
+    }),
+    typescript2({
+      clean: true,
+      tsconfigDefaults: {
+        exclude: [
+          '**/*.spec.ts',
+          '**/*.test.ts',
+          '**/*.spec.tsx',
+          '**/*.test.tsx',
+          'node_modules',
+          'dist'
         ],
-        extensions: ['.css', '.scss', '.sass'],
-        use: ['sass']
-      }),
-      typescript2({
-        clean: true,
-        tsconfigDefaults: {
-          exclude: [
-            '**/*.spec.ts',
-            '**/*.test.ts',
-            '**/*.spec.tsx',
-            '**/*.test.tsx',
-            'node_modules',
-            'dist'
-          ],
-          compilerOptions: {
-            rootDir: './src',
-            sourceMap: true,
-            declaration: true,
-            jsx: 'react',
-            target: 'es5',
-            lib: ['dom', 'dom.iterable', 'esnext'],
-            allowJs: true,
-            skipLibCheck: true,
-            esModuleInterop: true,
-            allowSyntheticDefaultImports: true,
-            strict: true,
-            forceConsistentCasingInFileNames: true,
-            module: 'esnext',
-            moduleResolution: 'node',
-            resolveJsonModule: true,
-            isolatedModules: true,
-            noEmit: true
-          }
+        compilerOptions: {
+          rootDir: './src',
+          sourceMap: true,
+          declaration: true,
+          jsx: 'react',
+          target: 'es5',
+          lib: ['dom', 'dom.iterable', 'esnext'],
+          allowJs: true,
+          skipLibCheck: true,
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+          strict: true,
+          forceConsistentCasingInFileNames: true,
+          module: 'esnext',
+          moduleResolution: 'node',
+          resolveJsonModule: true,
+          isolatedModules: true,
+          noEmit: true
         }
-      }),
-      commonjs({ include: /\/node_modules\// }),
-      resolve({
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        preferBuiltins: false,
-        browser: true
-      }),
-      babel({
-        babelHelpers: 'runtime',
-        skipPreflightCheck: true,
-        babelrc: false,
-        extensions: ['.js', 'jsx', '.ts', '.tsx'],
-        presets: ['@babel/preset-env', '@babel/preset-react'],
-        plugins: [
-          '@babel/plugin-transform-runtime',
-          '@babel/proposal-class-properties'
-        ],
-        exclude: /\/node_modules\//
-      }),
-      stripCode({
-        start_comment: 'placeholder:start',
-        end_comment: 'placeholder:end'
-      })
-    ]
-  };
-};
+      }
+    }),
+    commonjs({ include: /\/node_modules\// }),
+    resolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      preferBuiltins: false,
+      browser: true
+    }),
+    babel({
+      babelHelpers: 'runtime',
+      skipPreflightCheck: true,
+      babelrc: false,
+      extensions: ['.js', 'jsx', '.ts', '.tsx'],
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: [
+        '@babel/plugin-transform-runtime',
+        '@babel/proposal-class-properties'
+      ],
+      exclude: /\/node_modules\//
+    }),
+    stripCode({
+      start_comment: 'placeholder:start',
+      end_comment: 'placeholder:end'
+    })
+  ]
+});
