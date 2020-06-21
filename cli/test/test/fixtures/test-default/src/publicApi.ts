f42782363038a0a9c474a56a8a61d7e91088931a
maktarsis <tarsis.maksym@gmail.com>
2020-06-21 13:54:00 +0300

    test(test): cover default testing

---
+++ cli/test/test/fixtures/test-default/src/publicApi.ts
@@ -0,0 +1,5 @@
+export const noop = (): void => {};
+
+export const sum = (a: number, b: number): number => a + b;
+
+export const concat = (a: string, b: string): string => a + b;
---
+++ cli/test/test/test-default.test.ts
@@ -0,0 +1,40 @@
+import * as shell from 'shelljs';
+
+import {
+  setupStage,
+  teardownStage
+} from '../../src/shared/utils/fixture.utils';
+import { execWithCache } from '../../src/shared/utils/shell.utils';
+
+shell.config.silent = false;
+
+const testDir = 'test';
+const fixtureName = 'test-default';
+
+describe('[bin.test.default]', () => {
+  beforeAll(() => {
+    teardownStage(fixtureName);
+    setupStage(testDir, fixtureName);
+  });
+
+  afterAll(() => {
+    teardownStage(fixtureName);
+  });
+
+  it('should finish positive tests', () => {
+    const output = execWithCache('node ../dist/src/bin/index.js test');
+    expect(output.code).toBe(0);
+  });
+
+  it('should not find any test with pattern arg and finish with code 1', () => {
+    const output = execWithCache('node ../dist/src/bin/index.js test patternTest');
+    expect(output.code).toBe(1);
+  });
+
+  it('should not find any test with pattern arg and passWithNoTests option and finish with code 0', () => {
+    const output = execWithCache(
+      'node ../dist/src/bin/index.js test patternTest --passWithNoTests'
+    );
+    expect(output.code).toBe(0);
+  });
+});
