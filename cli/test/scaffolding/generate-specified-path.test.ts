d2a2f7e3e734f90500553a16d0b04da6bf8f67e4
maktarsis <tarsis.maksym@gmail.com>
2020-06-24 20:44:11 +0300

    test(generate): cover specified path when generating new package

---
+++ cli/test/scaffolding/generate-specified-path.test.ts
@@ -0,0 +1,77 @@
+import * as shell from 'shelljs';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
+import {
+  setupStage,
+  teardownStage
+} from '../../src/shared/utils/fixture.utils';
+import { smartExec } from '../../src/shared/utils/shell.utils';
+
+shell.config.silent = false;
+const testDir = 'scaffolding';
+const fixtureName = 'generate-specified-path';
+const template = 'generate';
+
+describe('[bin.scaffolding.generate-specified-path]', () => {
+  beforeAll(() => {
+    teardownStage(fixtureName);
+    setupStage(testDir, fixtureName, template);
+  });
+
+  afterAll(() => {
+    teardownStage(fixtureName);
+  });
+
+  it('should have generated package by the specified path + package name', () => {
+    const output = smartExec(
+      'node ../../../dist/src/bin/index.js generate myPackage packageDir --template basic'
+    );
+    expect(shell.test('-d', 'packageDir/myPackage')).toBeTruthy();
+    expect(output.code).toBe(0);
+  });
+
+  it('should have updated workspaces declaration', () => {
+    const output = smartExec(
+      'node ../../../dist/src/bin/index.js generate myPackage packageDir --template basic'
+    );
+    const rootPackageJson = fs.readJSONSync(path.resolve('package.json'));
+    expect(rootPackageJson.workspaces).toContain('packageDir/myPackage');
+    expect(output.code).toBe(0);
+  });
+
+  it('should have generated package by the specified path + package name', () => {
+    const output = smartExec(
+      'node ../../../dist/src/bin/index.js generate myPackage packageDir --template basic'
+    );
+    expect(shell.test('-d', 'packageDir/myPackage')).toBeTruthy();
+    expect(output.code).toBe(0);
+  });
+
+  it('should generate package in the root', () => {
+    const output = smartExec(
+      'node ../../../dist/src/bin/index.js generate myPackage . --template basic'
+    );
+    expect(shell.test('-d', 'myPackage')).toBeTruthy();
+    expect(output.code).toBe(0);
+  });
+
+  it('should have updated workspaces declaration with package in the root', () => {
+    const output = smartExec(
+      'node ../../../dist/src/bin/index.js generate myPackage . --template basic'
+    );
+    const rootPackageJson = fs.readJSONSync(path.resolve('package.json'));
+    expect(rootPackageJson.workspaces).toContain('myPackage');
+    expect(output.code).toBe(0);
+  });
+
+  it('should generate deep nested package', () => {
+    const output = smartExec(
+      'node ../../../dist/src/bin/index.js generate myPackage packages/components/helpers --template basic'
+    );
+    expect(
+      shell.test('-d', 'packages/components/helpers/myPackage')
+    ).toBeTruthy();
+    expect(output.code).toBe(0);
+  });
+});
