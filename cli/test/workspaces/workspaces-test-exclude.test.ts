26ad6106e39a6bb64fdf220ac47944b6e3af81e7
maktarsis <tarsis.maksym@gmail.com>
2020-06-27 00:12:57 +0300

    test(workspaces): cover exclude test option

---
+++ cli/test/workspaces/workspaces-test-exclude.test.ts
@@ -0,0 +1,26 @@
+import * as shell from 'shelljs';
+
+import { setupStage, teardownStage } from '../../src/shared/utils/fixture.utils';
+import { smartExec } from '../../src/shared/utils/shell.utils';
+
+shell.config.silent = false;
+
+const testDir = 'workspaces';
+const fixtureName = 'workspaces-test-exclude';
+const template = 'workspaces-test-invalid';
+
+describe('[bin.execution.workspaces-test-exclude]', () => {
+  beforeAll(() => {
+    teardownStage(fixtureName);
+    setupStage(testDir, fixtureName, template);
+  });
+
+  afterAll(() => {
+    teardownStage(fixtureName);
+  });
+
+  it('should exclude failed test and exit with 0', () => {
+    const output = smartExec('node ../../../dist/src/bin/index.js workspaces test --exclude workspaces-example-2');
+    expect(output.code).toBe(1);
+  });
+});
