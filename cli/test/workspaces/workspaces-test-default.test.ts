d3ddaefd9f92413525a57c7c0a8c9e7249fe910f
maktarsis <tarsis.maksym@gmail.com>
2020-06-27 00:11:14 +0300

    test(workspaces): cover default test

---
+++ cli/test/workspaces/workspaces-test-default.test.ts
@@ -0,0 +1,25 @@
+import * as shell from 'shelljs';
+
+import { setupStage, teardownStage } from '../../src/shared/utils/fixture.utils';
+import { smartExec } from '../../src/shared/utils/shell.utils';
+
+shell.config.silent = false;
+
+const testDir = 'workspaces';
+const fixtureName = 'workspaces-test-default';
+
+describe('[bin.execution.workspaces-test-default]', () => {
+  beforeAll(() => {
+    teardownStage(fixtureName);
+    setupStage(testDir, fixtureName);
+  });
+
+  afterAll(() => {
+    teardownStage(fixtureName);
+  });
+
+  it('should test packages with exit 0 when all packages have passed tests', () => {
+    const output = smartExec('node ../../../dist/src/bin/index.js workspaces test');
+    expect(output.code).toBe(0);
+  });
+});
