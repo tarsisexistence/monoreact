07906c6a92081fd8a53f58eed1752890130967c1
maktarsis <tarsis.maksym@gmail.com>
2020-06-26 11:09:03 +0300

    test(new): cover all what can be tested at the moment for new.bin

---
+++ cli/test/scaffolding/new-cra.test.ts
@@ -0,0 +1,52 @@
+import * as shell from 'shelljs';
+
+import { setupStage, teardownStage } from '../../src/shared/utils/fixture.utils';
+import { smartExec } from '../../src/shared/utils/shell.utils';
+
+shell.config.silent = false;
+const testDir = 'scaffolding';
+const fixtureName = 'new-cra';
+const template = 'new';
+
+describe('[bin.scaffolding.new-cra]', () => {
+  beforeAll(() => {
+    teardownStage(fixtureName);
+    setupStage(testDir, fixtureName, template);
+  });
+
+  afterAll(() => {
+    teardownStage(fixtureName);
+  });
+
+  it('should create new project', () => {
+    const output = smartExec('node ../../../dist/src/bin/index.js new myProject --template cra');
+    expect(shell.test('-d', 'myProject')).toBeTruthy();
+    expect(output.code).toBe(0);
+  });
+
+  it('should have project directories', () => {
+    const output = smartExec('node ../../../dist/src/bin/index.js new myProject --template cra');
+    expect(shell.test('-d', 'myProject/public')).toBeTruthy();
+    expect(shell.test('-d', 'myProject/src')).toBeTruthy();
+    expect(output.code).toBe(0);
+  });
+
+  it('should have necessary project files', () => {
+    const output = smartExec('node ../../../dist/src/bin/index.js new myProject --template cra');
+    expect(shell.test('-f', 'myProject/package.json')).toBeTruthy();
+    expect(shell.test('-f', 'myProject/tsconfig.json')).toBeTruthy();
+    expect(shell.test('-f', 'myProject/README.md')).toBeTruthy();
+    expect(shell.test('-f', 'myProject/.gitignore')).toBeTruthy();
+    expect(output.code).toBe(0);
+  });
+
+  it('should have additional boilerplate config files', () => {
+    const output = smartExec('node ../../../dist/src/bin/index.js new myProject --template cra');
+    expect(shell.test('-f', 'myProject/.eslintrc.js')).toBeTruthy();
+    expect(shell.test('-f', 'myProject/.huskyrc.json')).toBeTruthy();
+    expect(shell.test('-f', 'myProject/.lintstagedrc.json')).toBeTruthy();
+    expect(shell.test('-f', 'myProject/.prettierrc.json')).toBeTruthy();
+    expect(shell.test('-f', 'myProject/.stylelintrc.json')).toBeTruthy();
+    expect(output.code).toBe(0);
+  });
+});
---
+++ cli/test/scaffolding/new-invalid-template.test.ts
@@ -0,0 +1,31 @@
+import * as shell from 'shelljs';
+
+import { setupStage, teardownStage } from '../../src/shared/utils/fixture.utils';
+import { smartExec } from '../../src/shared/utils/shell.utils';
+
+shell.config.silent = false;
+const testDir = 'scaffolding';
+const fixtureName = 'new-invalid-template';
+const template = 'new';
+
+describe('[bin.scaffolding.new-invalid-template]', () => {
+  beforeAll(() => {
+    teardownStage(fixtureName);
+    setupStage(testDir, fixtureName, template);
+  });
+
+  afterAll(() => {
+    teardownStage(fixtureName);
+  });
+
+  // TODO: provide select with available templates instead exit 1
+  it('should fail with exit code 1', () => {
+    const output = smartExec('node ../../../dist/src/bin/index.js new myProject --template someInvalidTemplateName');
+    expect(output.code).toBe(1);
+  });
+
+  it('should not create new project', () => {
+    smartExec('node ../../../dist/src/bin/index.js new myProject --template someInvalidTemplateName');
+    expect(shell.test('-d', 'myProject')).toBeFalsy();
+  });
+});
