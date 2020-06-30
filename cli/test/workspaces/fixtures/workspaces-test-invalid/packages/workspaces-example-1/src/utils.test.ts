9f9fbf7a6630a575d34baa86031aa90bb3d6efd0
maktarsis <tarsis.maksym@gmail.com>
2020-06-30 20:32:36 +0300

    fix: test of invalid excluded workspace

--- cli/test/workspaces/fixtures/workspaces-test-invalid/packages/workspaces-example-1/src/utils.test.ts
+++ cli/test/workspaces/fixtures/workspaces-test-invalid/packages/workspaces-example-1/src/utils.test.ts
@@ -2,7 +2,7 @@ import { sum } from './utils';
 
 describe('test-config sum', () => {
   it('should return sum of 1 and 2', () => {
-    expect(sum(1, 2)).toBe(4);
+    expect(sum(1, 2)).toBe(3);
   });
 
   it('should fail when is not correct', () => {
