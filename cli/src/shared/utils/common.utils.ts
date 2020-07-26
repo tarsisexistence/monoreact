6e5769c61f442eaf65f833aff2e908a914319da1
maktarsis <tarsis.maksym@gmail.com>
2020-07-26 13:39:03 +0300

    refactor(author): update npm author utils

--- cli/src/shared/utils/common.utils.ts
+++ cli/src/shared/utils/common.utils.ts
@@ -13,3 +13,44 @@ export const installDependencies = (): ShellString => exec('yarn install', { sil
 
 /* eslint-disable no-empty-function, @typescript-eslint/no-empty-function */
 export const noop = (): void => {};
+
+export function setNpmAuthorName(author: CLI.Package.Author): void {
+  exec(`npm config set init-author-name "${author}"`, { silent: true });
+}
+
+export function getNpmAuthorName(): CLI.Package.Author | null {
+  const npmNameAuthor = exec('npm config get init-author-name', {
+    silent: true
+  }).stdout.trim();
+
+  if (npmNameAuthor) {
+    return npmNameAuthor;
+  }
+
+  const gitNameAuthor = exec('git config user.name', { silent: true }).stdout.trim();
+
+  if (gitNameAuthor) {
+    setNpmAuthorName(gitNameAuthor);
+    return gitNameAuthor;
+  }
+
+  const npmEmailAuthor = exec('npm config get init-author-email', {
+    silent: true
+  }).stdout.trim();
+
+  if (npmEmailAuthor) {
+    return npmEmailAuthor;
+  }
+
+  const gitEmailAuthor = exec('git config user.email', { silent: true }).stdout.trim();
+
+  if (gitEmailAuthor) {
+    return gitEmailAuthor;
+  }
+
+  return null;
+}
+
+export function deleteNpmAuthorName(author: CLI.Package.Author): void {
+  exec(`npm config delete "${author}"`, { silent: true });
+}
