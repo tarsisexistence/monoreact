c29f6cbe77ff553126882b16f1f280bea5a12f66
retarsis <tarsis.maksym@gmail.com>
2020-12-05 17:17:24 +0200

    chore: remove moduleNameMapper leftovers

--- cli/src/shared/constants/package.const.ts
+++ cli/src/shared/constants/package.const.ts
@@ -89,8 +89,5 @@ export const WORKSPACE_ROOT_PACKAGE_JSON: CLI.Package.HostPackageJSON = {
   browserslist: {
     production: ['>0.2%', 'not dead', 'not op_mini all'],
     development: ['last 1 chrome version', 'last 1 firefox version', 'last 1 safari version']
-  },
-  jest: {
-    moduleNameMapper: {}
   }
 };
