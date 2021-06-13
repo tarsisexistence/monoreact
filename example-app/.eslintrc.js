712843f0e83a1b55371065932ffa411f6db6bb79
retarsis <tarsis.maksym@gmail.com>
2021-06-13 09:17:13 +0300

    chore: remove deprecated lint "import/imports-first" rule

--- example-app/.eslintrc.js
+++ example-app/.eslintrc.js
@@ -94,7 +94,6 @@ module.exports = {
     'import/default': 2,
     'import/export': 2,
     'import/no-cycle': 2,
-    'import/imports-first': [2, 'absolute-first'],
     'import/newline-after-import': 2,
     'import/prefer-default-export': 0,
     'import/no-useless-path-segments': 2,
