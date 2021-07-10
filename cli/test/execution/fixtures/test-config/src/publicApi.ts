35dae9c1ded9ae89281d68f1b3bec821892033fb
retarsis <tarsis.maksym@gmail.com>
2021-07-10 15:43:07 +0300

    refactor: noop

--- cli/test/execution/fixtures/test-config/src/publicApi.ts
+++ cli/test/execution/fixtures/test-config/src/publicApi.ts
@@ -1,3 +1,3 @@
-export const noop = (): void => {};
+export const noop = (): void => void 0;
 
 export const sum = (a: number, b: number): number => a + b;
--- cli/test/execution/fixtures/test-default/src/publicApi.ts
+++ cli/test/execution/fixtures/test-default/src/publicApi.ts
@@ -1,4 +1,4 @@
-export const noop = (): void => {};
+export const noop = (): void => void 0;
 
 export const sum = (a: number, b: number): number => a + b;
 
