94e7eaaf3595b6baf6d7c793a1ae2c0db6508494
retarsis <tarsis.maksym@gmail.com>
2021-07-10 15:42:35 +0300

    chore: refresh detach deps

--- cli/src/bin/migration/setup/detach.setup.ts
+++ cli/src/bin/migration/setup/detach.setup.ts
@@ -1,18 +1,18 @@
 export const detachSetup: CLI.Setup.MigrationOptions = {
   dependencies: {
-    react: '^17.0.1',
-    'react-dom': '^17.0.1'
+    react: '^17.0.2',
+    'react-dom': '^17.0.2'
   },
   devDependencies: {
     monoreact: 'latest',
-    '@typescript-eslint/parser': '^4.24.0',
-    '@types/react': '^17.0.6',
-    '@types/react-dom': '^17.0.5',
+    '@typescript-eslint/parser': '^4.28.2',
+    '@types/react': '^17.0.14',
+    '@types/react-dom': '^17.0.9',
     'eslint-config-react-app': '^6.0.0',
     husky: '^4.3.5',
     'lint-staged': '11.0.0',
-    prettier: '2.3.0',
-    tslib: '^2.2.0',
+    prettier: '2.3.2',
+    tslib: '^2.3.0',
     typescript: '^4.3.5'
   }
 };
