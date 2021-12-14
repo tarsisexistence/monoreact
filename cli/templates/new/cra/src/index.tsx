113b7ec2bd6cc2057638926f66396983b2abf9fe
Max Tarsis <21989873+untaggable@users.noreply.github.com>
2021-12-14 20:48:42 +0200

    feat: update "monoreact new" template/skeleton

--- cli/templates/new/cra/src/index.tsx
+++ cli/templates/new/cra/src/index.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './App/App';
-import { unregister } from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
