927e216806ca61c8b826c137edd2f5bfa3d55f46
Max Tarsis <21989873+untaggable@users.noreply.github.com>
2021-12-20 09:27:35 +0200

    fix(build): broken build regression

--- cli/rollup.config.js
+++ cli/rollup.config.js
@@ -2,8 +2,8 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import beep from '@rollup/plugin-beep';
-import replace from '@rollup/plugin-replace';
 import closure from '@ampproject/rollup-plugin-closure-compiler';
+import { preserveShebangs } from 'rollup-plugin-preserve-shebangs';
 import esbuild from 'rollup-plugin-esbuild';
 import progress from 'rollup-plugin-progress';
 import globals from 'rollup-plugin-node-globals';
@@ -11,7 +11,6 @@ import builtins from 'rollup-plugin-node-builtins';
 import packageJson from './package.json';
 
 export default {
-  banner: '#!/usr/bin/env node',
   input: 'src/bin/index.ts',
   output: [
     { file: 'dist/bundle.js', format: 'es' },
@@ -24,14 +23,10 @@ export default {
     return id => externalsMap.has(id) || Boolean(externals.find(key => id.startsWith(`${key}/`)));
   })(packageJson),
   plugins: [
+    preserveShebangs(),
     beep(),
     progress({ clearLine: true }),
     json(),
-    replace({
-      preventAssignment: true,
-      delimiters: ['', ''],
-      '#!/usr/bin/env node': ''
-    }),
     esbuild({
       include: /\.ts$/,
       exclude: /node_modules/,
@@ -39,14 +34,14 @@ export default {
       minify: true,
       target: 'es2019' // default, or 'es20XX', 'esnext'
     }),
-    closure(), // optional
     commonjs({ include: /\/node_modules\//, sourceMaps: false }),
     nodeResolve({
       extensions: ['.js', '.ts'],
       preferBuiltins: true,
       browser: false
     }),
-    globals(),
-    builtins()
+    closure(), // optional?
+    globals(), // optional?
+    builtins() // optional?
   ]
 };
