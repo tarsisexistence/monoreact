7f9c914bd26894e4c49febd4d3b56e6797770d51
Max Tarsis <21989873+untaggable@users.noreply.github.com>
2021-12-25 21:13:17 +0200

    feat: add axe-mode

--- example-app/src/App/App.tsx
+++ example-app/src/App/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Products } from '@monoreact/package-example';
 import { DatePicker } from '@tarsis/datepicker';
+import AxeMode from 'axe-mode';
 
 import styles from './App.module.css';
 
 export const App: React.FC = () => (
-  <div className={styles.container}>
-    <Products />
-    Monoreact: react workspace groundwork
-    <DatePicker />
-  </div>
+  <AxeMode disabled={process.env.NODE_ENV !== 'development'}>
+    <div className={styles.container}>
+      <Products />
+      Monoreact: react workspace groundwork
+      <DatePicker />
+    </div>
+  </AxeMode>
 );
