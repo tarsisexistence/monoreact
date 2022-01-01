7141380bfc0cd0cbff7841349f519a97f52fe099
Max Tarsis <21989873+untaggable@users.noreply.github.com>
2022-01-01 20:41:27 +0200

    chore: remove axe-mode

--- example-app/src/App/App.tsx
+++ example-app/src/App/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Products } from '@monoreact/package-example';
 import { DatePicker } from '@tarsis/datepicker';
-import AxeMode from 'axe-mode';
 
 import styles from './App.module.css';
 
 export const App: React.FC = () => (
-  <AxeMode disabled={process.env.NODE_ENV !== 'development'}>
-    <div className={styles.container}>
-      <Products />
-      Monoreact: react workspace groundwork
-      <DatePicker />
-    </div>
-  </AxeMode>
+  <div className={styles.container}>
+    <Products />
+    Monoreact: react workspace groundwork
+    <DatePicker />
+  </div>
 );
